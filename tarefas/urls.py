31a8b279ca78da567c0655e8e0d7784c3550e212
Benlove Anelus <61205994+benlove-chris@users.noreply.github.com>
2025-01-06 23:34:52 -0300

    configurando token
    adição das rotas do token jwt

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,15 +1,26 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from .views import TarefaViewSet
 from .user_views import criar_usuario
 
+# Definindo o roteador para a API de tarefas
 router = DefaultRouter()
 router.register(r'tarefas', TarefaViewSet, basename='tarefa')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('signup/', criar_usuario, name='signup'),
-]
+    # Rota de autenticação JWT
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-# Definindo o roteador para a API
-# O caminho para a API de tarefas será 'api/tarefas/'
+    # Rota para criar usuário
+    path('signup/', criar_usuario, name='signup'),
+
+    # Rota para tarefas
+    path('', include(router.urls)),  # Rotas de tarefas
+]
--- todolist/urls.py
+++ todolist/urls.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
-from rest_framework.authtoken import views
 from django.urls import path, include
-"""
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('tarefas.urls')),  # Inclui as rotas da API
-    path('api/token/', views.obtain_auth_token, name='api_token_auth'),
-    
-]
-"""
-from django.contrib import admin
-from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
+# Configuração da documentação Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentação",
+        default_version='v1',
+        description="Documentação da API",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+# Rotas principais do projeto
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Rota para o admin do Django
-    path('api/', include('tarefas.urls')),  # Inclui as URLs do app `tarefas`
+    path('admin/', admin.site.urls),  # Rota do admin do Django
+    path('api/', include('tarefas.urls')),  # Inclui as rotas do app `tarefas`
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Documentação Swagger
 ]
