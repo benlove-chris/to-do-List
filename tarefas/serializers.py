9d5a3eded377b8987fb513f0739d990515fc41ff
Benlove Anelus <61205994+benlove-chris@users.noreply.github.com>
2025-02-10 22:48:47 -0300

    Criação de tarefa completa
    depois de logado, para criar uma tarefa precisa apenas do "titulo",
        "descricao",
        "status".

--- tarefas/serializers.py
+++ tarefas/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import Tarefa
-from django.contrib.auth.models import User
 
+# Definimos um serializer para o modelo Tarefa
 class TarefaSerializer(serializers.ModelSerializer):
-    usuario = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  # Relacionamento com o modelo User
-    
+    # O campo 'usuario' é definido como somente leitura e seu valor padrão é o usuário atual
+    usuario = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
+        # Especificamos que este serializer está associado ao modelo Tarefa
         model = Tarefa
+        # Definimos os campos que serão incluídos na serialização
         fields = ['id', 'titulo', 'descricao', 'status', 'usuario', 'd_criacao', 'd_atualizacao']
+        # Especificamos quais campos são somente leitura
+        read_only_fields = ['id', 'd_criacao', 'd_atualizacao']
