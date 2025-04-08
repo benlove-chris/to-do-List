9627f317932afe738c654caaa2985faed04f2786
Benlove Anelus <61205994+benlove-chris@users.noreply.github.com>
2025-04-07 22:22:41 -0300

    configurando api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/', // Ajuste conforme necessário
+  //baseURL: 'http://127.0.0.1:8000/api/', // Local
+  baseURL: 'https://benanywhere.pythonanywhere.com/api/', //online
 });
 
 export default api;
